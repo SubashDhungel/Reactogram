257f232ea6e28726ec8ca0547ac17d722771a780
Nirajan Dhungel <subashdhungel555@gmail.com>
2024-07-25 13:05:42 +0545

    Added files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="" alt="" />
+      <ul>
+      <li>Home</li>
+      <li>Program</li>
+      <li>About Us</li>
+      <li>Campus</li>
+      <li>Testimonials</li>
+      <li>Contact Us</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
